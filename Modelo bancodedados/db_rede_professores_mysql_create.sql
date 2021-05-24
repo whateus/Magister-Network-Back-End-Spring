a0dfd6d9496b0f216f6a1354694a7e0d0fe8fe5b
viniciusmartins21 <viniciusmartins.12@hotmail.com>
2021-05-24 11:39:40 -0300

    Task 02

---
+++ Modelo bancodedados/db_rede_professores_mysql_create.sql
@@ -0,0 +1,33 @@
+CREATE TABLE `tb_postagem` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`tema_id` INT NOT NULL AUTO_INCREMENT,
+	`usuario_id` INT NOT NULL AUTO_INCREMENT,
+	`descricao` varchar(255) NOT NULL,
+	`titulo` varchar(150) NOT NULL,
+	`data_postagem` DATE NOT NULL,
+	`midia` varchar(400) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `tb_tema` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`titulo_referencia` varchar(100) NOT NULL,
+	`categoria` varchar(100) NOT NULL,
+	`disciplina` varchar(100) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `tb_usuario` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`nome` varchar(100) NOT NULL,
+	`senha` varchar(11) NOT NULL,
+	`email` varchar(100) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+ALTER TABLE `tb_postagem` ADD CONSTRAINT `tb_postagem_fk0` FOREIGN KEY (`id`) REFERENCES ``(``);
+
+ALTER TABLE `tb_postagem` ADD CONSTRAINT `tb_postagem_fk1` FOREIGN KEY (`tema_id`) REFERENCES `tb_tema`(`id`);
+
+ALTER TABLE `tb_postagem` ADD CONSTRAINT `tb_postagem_fk2` FOREIGN KEY (`usuario_id`) REFERENCES `tb_usuario`(`id`);
+
