15e32723c93f4c80c954bad12d0d395ca64b8f75
jeytoyota <jeytoyota@gmail.com>
2021-06-14 10:00:45 -0300

    atualiza

--- magisterNetwork/src/main/java/com/generation/magisterNetwork/service/UsuarioService.java
+++ magisterNetwork/src/main/java/com/generation/magisterNetwork/service/UsuarioService.java
@@ -44,7 +44,7 @@ public class UsuarioService {
 
 				user.get().setToken(authHeader);
 				user.get().setNome(usuario.get().getNome());
-				//user.get().setSenha(usuario.get().getSenha());
+				user.get().setSenha(usuario.get().getSenha());
 
 				return user;
 			}
