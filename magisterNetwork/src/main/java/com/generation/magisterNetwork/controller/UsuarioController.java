15cdb4ddea3fda954f08aceb91fcffca66991080
jeytoyota <jeytoyota@gmail.com>
2021-07-10 16:13:12 -0300

    atualização do back-end

--- magisterNetwork/src/main/java/com/generation/magisterNetwork/controller/UsuarioController.java
+++ magisterNetwork/src/main/java/com/generation/magisterNetwork/controller/UsuarioController.java
@@ -6,13 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.magisterNetwork.model.Usuario;
 import com.generation.magisterNetwork.model.UsuarioLogin;
+import com.generation.magisterNetwork.repository.UsuarioRepository;
 import com.generation.magisterNetwork.service.UsuarioService;
 
 @RestController
@@ -22,6 +26,10 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	@Autowired
+	private UsuarioRepository repository;
+
 	
 	@PostMapping("/logar")
 	
@@ -35,5 +43,15 @@ public class UsuarioController {
 	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(usuarioService.CadastrarUsuario(usuario));
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> GetById(@PathVariable long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PutMapping
+	public ResponseEntity<Usuario> Put(@RequestBody Usuario usuario) {
+		return ResponseEntity.ok(repository.save(usuario));
 	}
 }
--- magisterNetwork/src/main/java/com/generation/magisterNetwork/security/BasicSecurityConfig.java
+++ magisterNetwork/src/main/java/com/generation/magisterNetwork/security/BasicSecurityConfig.java
@@ -36,6 +36,7 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
 		http.authorizeRequests()
 		.antMatchers("/usuarios/logar").permitAll()
 		.antMatchers("/usuarios/cadastrar").permitAll()
+		.antMatchers("/usuarios/{id}").permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
 		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
